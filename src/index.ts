be062191f579a8c2d82b9bff51b4f63ca8066923
Lukas Möller <13099043+moeller-projects@users.noreply.github.com>
2023-02-01 07:55:56 +0000

    updated version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello from my-script👋')
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+async function run() {
+  const program = new Command();
+
+  program
+    .argument('<url>', 'Lighthouse will run the analysis on the URL.')
+    .option(
+      '-i, --iteration <type>',
+      'How many times Lighthouse should run the analysis per URL',
+      '5',
+    )
+    .parse();
+
+  const [url] = program.args;
+  const options = program.opts();
+
+  console.log(`url: ${url}, iteration: ${options.iteration}`);
+}
+
+run();
